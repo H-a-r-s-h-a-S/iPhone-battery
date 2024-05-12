3b53e6a9b510e3f1d15322237a892db97a4b4d95
Harsha S <harsha@debian.home>
2024-05-12 13:55:51 +0530

    add steps

---
+++ steps.sh
@@ -0,0 +1,22 @@
+echo "creating schema script"
+bash create\ table.sh schema.csv
+
+echo "creating schema"
+mysql -u root -p battery < schema.sql
+
+echo "extracting data"
+python3 charging.py
+
+echo "loading data"
+mysql --local-infile=1 -u root -p battery < load.sql
+
+sudo rm /var/lib/mysql-files/final.csv
+
+echo "cleaning data"
+mysql -u root -p battery < clean.sql
+
+sudo mv /var/lib/mysql-files/final.csv final.csv
+sudo chmod ugo+rw final.csv
+
+mongoimport -d battery -c charging --file charging.json --drop -v
+
